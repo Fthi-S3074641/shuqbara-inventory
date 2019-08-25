a005d2597aeb8d365040bd9e56f6f2dee92741c4
Fthi-S3074641 <fthialem@gmail.com>
2019-08-25 13:42:25 +0300

    =corrected filtering

--- src/components/read.vue
+++ src/components/read.vue
@@ -5,16 +5,18 @@
 
       <v-card-title >
       <h2 class="mb-1 headline black--text">&nbsp;&nbsp;&nbsp;&nbsp; </h2>
-            <v-autocomplete
+          <v-text-field
             label="Search using a unique code"
-            placeholder="Write down ID or code of an item"
-            :items="getCodeList"
-            prepend-inner-icon="mdi-magnify"
-            ></v-autocomplete>
+            placeholder="Write down Id"
+            prepend-icon="mdi-magnify"
+            clearable
+            required
+            v-model="searchString"
+          ></v-text-field>
       </v-card-title>
 
     <v-list three-line>
-      <template v-for="(item, index) in getAll">
+      <template v-for="(item, index) in filteredArticles">
 
         <v-divider
           :key="index"
@@ -24,18 +26,28 @@
         <v-list-item
           :key="item.icode"
         >
-          <v-list-item-avatar>
-            <v-badge left color="purple">
-                <template v-slot:badge>
-                    {{item.iquantity}}
-                </template>
-            </v-badge>
+          <v-list-item-avatar color="primary" size="36">
+            <span class="white--text title font-weight-light">{{item.iquantity}}</span>
           </v-list-item-avatar>
 
           <v-list-item-content>
-            <v-list-item-title v-html="item.icode"></v-list-item-title>
+            <v-list-item-title class="title font-weight-light" v-html="item.icode"></v-list-item-title>
             <v-list-item-subtitle v-html="item.idetail"></v-list-item-subtitle>
           </v-list-item-content>
+
+          <v-list-item-action>
+            <v-list-item-action-text v-text="`Created at: ${item.iregister}`"></v-list-item-action-text>
+            <v-row justify="space-around">
+          <v-btn icon>
+            <v-icon>mdi-pencil</v-icon>
+          </v-btn>
+          <v-btn icon>
+            <v-icon>mdi-delete</v-icon>
+          </v-btn>
+          </v-row>
+        </v-list-item-action>
+
+
         </v-list-item>
       </template>
     </v-list>
@@ -46,8 +58,17 @@
 
 <script>
   export default {
-    data: () => ({
-    }),
+    data() {
+      return {
+        searchString: null
+      }
+    },
+    methods: {
+      getIndex(iicode) {
+        return this.getAll.map(function(e) {
+          return e.icode;}).indexOf(iicode);
+      }
+    },
     computed: {
       getCodeList(){
         return this.getAll.map(fruit => fruit.icode)
@@ -55,7 +76,26 @@
       getAll() {
         // console.log(this.$store.state.allItems)
         return this.$store.state.allItems
-      }
+      },        
+      
+      filteredArticles: function () {
+            var articles_array = this.getAll,
+                searchString = this.searchString;
+
+            if(!searchString){
+                return articles_array;
+            }
+
+            searchString = searchString.trim().toLowerCase();
+
+            articles_array = articles_array.filter(function(item){
+                if(item.icode.toLowerCase().indexOf(searchString) !== -1){
+                    return item;
+                }
+            })
+            // Return an array with the filtered data.
+            return articles_array
+        }
     }
   }
 </script>
