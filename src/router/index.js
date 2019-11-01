dede510e9c5efe1d1f130426123561b5153d5557
Fthi-S3074641 <fthialem@gmail.com>
2019-11-01 09:07:25 +0300

    =building proper chunks

--- src/router/index.js
+++ src/router/index.js
@@ -1,47 +1,43 @@
 import Vue from 'vue'
-import Vrouter from 'vue-router'
+import VueRouter from 'vue-router'
 
-// import welcome from './../views/welcome'
-// import register from './../views/register'
-// import read from './../views/read'
-// import report from './../views/report'
-// import comments from './../views/comments'
+Vue.use(VueRouter)
 
-Vue.use(Vrouter)
+const routes = [
+    {
+        path: '/',
+        name: '/welcome',
+        component: ()  => import(/* webpackChunkName: "welcome" */ '@/views/welcome.vue')
+    },
+    {
+        path: '/register',
+        name: '/register',
+        component: ()  => import(/* webpackChunkName: "register" */ '@/views/register.vue')
+    },
+    {
+        path: '/read',
+        name: '/read',
+        component: ()  => import(/* webpackChunkName: "read" */ '@/views/read.vue'),
+        props: true
+    },
+    {
+        path: '/report',
+        name: '/report',
+        component: ()  => import(/* webpackChunkName: "report" */ '@/views/report.vue'),
+        props: true
+    },
+    {
+        path: '/comments',
+        name: '/comments',
+        component: ()  => import(/* webpackChunkName: "comments" */ '@/views/comments.vue'),
+        props: true
+    }
+]
 
-export default new Vrouter({
-    scrollBehavior() {
-        return { x: 0, y: 0 };
-      },
-    routes: [
-        {
-            path: '/',
-            name: '/welcome',
-            component: ()  => import(/* webpackChunkName: "welcome" */ '@/views/welcome.vue')
-        },
-        {
-            path: '/register',
-            name: '/register',
-            component: ()  => import(/* webpackChunkName: "register" */ '@/views/register.vue')
-        },
-        {
-            path: '/read',
-            name: '/read',
-            component: ()  => import(/* webpackChunkName: "read" */ '@/views/read.vue'),
-            props: true
-        },
-        {
-            path: '/report',
-            name: '/report',
-            component: ()  => import(/* webpackChunkName: "report" */ '@/views/report.vue'),
-            props: true
-        },
-        {
-            path: '/comments',
-            name: '/comments',
-            component: ()  => import(/* webpackChunkName: "comments" */ '@/views/comments.vue'),
-            props: true
-        }
-    ],
-    mode: 'history'
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
 })
+
+export default router
